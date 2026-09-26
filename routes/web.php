04ef414e2b766a4fc0be1e0bd407690d704ec3b2
Romane <romane.gomis1989@gmail.com>
2026-09-26 11:29:52 +0200

    nettoyage routes

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,7 @@ use App\Http\Controllers\ContactController;
 
 //Accès à la racine de l'application
 Route::get('/', [HomeController::class, 'index'])->name('home');
-
-
-
 Route::get('/sons', [SonController::class, 'index'])->name('sons.index');
-
 Route::get('/wavesurfer', function() {return view('wavesurfer');});
 Route::get('/mentions', function() {return view('mentions');});
 Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
